aab6727a23acddc073ce701044a62d153f9960a3
Mbonea-Mjema <mjema86@gmail.com>
2022-12-29 12:44:31 +0300

    addded proxy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
+  async rewrites() {
+    return [
+      // we need to define a no-op rewrite to trigger checking
+      // all pages/static files before we attempt proxying
+      {
+        source: '/:path*',
+        destination: '/:path*',
+      },
+      {
+        source: '/:path*',
+        destination: `https://plugins.wese.co.tz/tg/:path*`,
+      },
+    ];
+  },
 }
-
-module.exports = nextConfig
